c14c84b3cc87ea9c30436732a618cb9f70978b32
Akshay-Gengaje <akshaysgengaje@gmail.com>
2023-11-22 11:15:41 +0530

    new file:   Projects/fast-pizza-app

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -22,36 +22,11 @@ const customerSlice = createSlice({
         state.createdAt = action.payload.createdAt;
       },
     },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
   },
 });
 
 export default customerSlice.reducer;
-export const { createCustomer } = customerSlice.actions;
-
-// export default function customerReducer(state = initialCustomer, action) {
-//   switch (action.type) {
-//     case "customer/create":
-//       return {
-//         ...state,
-//         fullName: action.payload.fullName,
-//         nationalId: action.payload.nationalId,
-//         createdAt: action.payload.createdAt,
-//       };
-//     default:
-//       return state;
-//   }
-// }
-
-// export function createCustomer(fullName, nationalId) {
-//   return {
-//     type: "customer/create",
-//     payload: { fullName, nationalId, createdAt: new Date().toISOString() },
-//   };
-// }
-
-// export function updateName(fullName) {
-//   return {
-//     type: "customer/update-name",
-//     payload: { fullName },
-//   };
-// }
+export const { createCustomer, updateName } = customerSlice.actions;
